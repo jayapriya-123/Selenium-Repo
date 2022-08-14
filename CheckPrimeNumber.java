9e01c90135209d75014ef2bfeafaeaae9d064774
jayapriya-123 <110734751+jayapriya-123@users.noreply.github.com>
2022-08-14 18:19:20 +0530

    Add files via upload

---
+++ CheckPrimeNumber.java
@@ -0,0 +1,25 @@
+package week1.day2;
+
+public class CheckPrimeNumber {
+	public static void main(String[] args) {
+		int n=19;
+		int count=0;
+		for(int i=1;i<=n;i++) {
+			if(n%i==0) {
+				count++;
+				
+		}
+	}
+	if(count==2) {
+	System.out.println("Number is prime");
+	}
+	else
+		System.out.println("Number is not prime");
+
+		}	
+				
+		}
+		
+	
+
+
